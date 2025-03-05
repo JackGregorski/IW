fcaf34554074a78321f534c2b73927f5b3a8955e
Jack Gregorski <jg9705@princeton.edu>
2025-03-05 10:34:16 -0500

    absolute paths

--- gen_encodings.sh
+++ gen_encodings.sh
@@ -15,13 +15,13 @@ module load anaconda3/2023.9
 conda activate sn-torch-env
 
 # Define the input FASTA file path
-INPUT_FASTA="scratch/gpfs/jg9705/IW_data/gen_ideal_protein_chem_scores/9606.protein.sequences.v12.0.fa"
+INPUT_FASTA="/scratch/gpfs/jg9705/IW_data/gen_ideal_protein_chem_scores/9606.protein.sequences.v12.0.fa"
 
 # Define the model file path
-MODEL_FILE="scratch/gpfs/jg9705/IW_data/gen_protein_encodings/esm1v_t33_650M_UR90S_1.pt"
+MODEL_FILE="/scratch/gpfs/jg9705/IW_data/gen_protein_encodings/esm1v_t33_650M_UR90S_1.pt"
 
 # Define the output file (include the array index in the filename)
-OUTPUT_FILE="scratch/gpfs/jg9705/IW_data/gen_protein_encodings/encodings_${SLURM_ARRAY_TASK_ID}.tsv"
+OUTPUT_FILE="/scratch/gpfs/jg9705/IW_data/gen_protein_encodings/encodings_${SLURM_ARRAY_TASK_ID}.tsv"
 
 echo "Running task ${SLURM_ARRAY_TASK_ID}: processing FASTA file ${INPUT_FASTA}"
 python gen_encodings.py "${INPUT_FASTA}" "${OUTPUT_FILE}" "${MODEL_FILE}"
