1c3a1ab3bc7492ec5fc4715c6ac53df8c0572b22
JackGregorski <158240098+JackGregorski@users.noreply.github.com>
2025-03-26 13:32:59 -0400

    more research

--- average_score.py
+++ average_score.py
@@ -3,8 +3,9 @@ import os
 
 pairs_df = pd.read_csv("/scratch/gpfs/jg9705/IW_data/gen_ideal_protein_chem_scores/9606.protein_chemical.links.v5.0.tsv", sep="\t")
 
-# Calculate the average score for each protein
-average_scores = pairs_df.groupby("protein")["combined_score"].mean().reset_index()
+# Calculate the average score for each protein and include how many times that protein appears in the dataset
+average_scores = pairs_df.groupby("protein").agg({"combined_score": "mean", "chemical": "count"}).reset_index()
+average_scores.columns = ["protein", "average_score", "count"]
 
 #sort the average scores in descending order
 average_scores = average_scores.sort_values(by="combined_score", ascending=False)
