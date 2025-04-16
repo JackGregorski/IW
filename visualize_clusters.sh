48636493b61d01f4c38834809b9dc5482e59da75
JackGregorski <158240098+JackGregorski@users.noreply.github.com>
2025-04-16 16:01:29 -0400

    attempting visualization

--- visualize_clusters.sh
+++ visualize_clusters.sh
@@ -12,13 +12,13 @@
 
 module purge
 module load anaconda3/2023.9
-conda activate sn-torch-env
+conda activate jg-torch-env
 
 echo "Starting cluster visualization..."
 
 # Set variables
-ENCODING_FILE="/scratch/gpfs/jg9705/IW_data/gen_protein_encodings/encodings/encodings.tsv"
-CLUSTER_FILE="/scratch/gpfs/jg9705/IW_data/Model_Resources/pairs_with_clusters.tsv"
+ENCODING_FILE="/scratch/gpfs/jg9705/IW_code/Model_Resources/encodings.tsv"
+CLUSTER_FILE="/scratch/gpfs/jg9705/IW_data/Model_Resources/pairs_with_clusters_visual.tsv"
 METHOD="pca"  # or tsne or umap
 SAVE_PATH="/scratch/gpfs/jg9705/IW_data/kmeans_clusters_${METHOD}.png"
 
