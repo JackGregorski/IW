7975fbf641bfdc8be93b650d67357e90341fd40f
JackGregorski <158240098+JackGregorski@users.noreply.github.com>
2025-04-24 09:42:31 -0400

    retry

--- eval_model.sh
+++ eval_model.sh
@@ -4,20 +4,20 @@
 #SBATCH --output=/scratch/gpfs/jg9705/IW_logs/%x.out
 #SBATCH --error=/scratch/gpfs/jg9705/IW_logs/%x.err
 #SBATCH --time=03:00:00
-#SBATCH --mem=80G
+#SBATCH --mem=128G
 #SBATCH --cpus-per-task=3
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=jg9705@princeton.edu
 #SBATCH -D /scratch/gpfs/jg9705/IW_code
 
-# Load appropriate environment
+
 module purge
 module load anaconda3/2023.9
 conda activate jg-torch-env
 
 echo "Starting evaluation of all models on all test sets..."
 
-# Run your evaluation script (assume it's called evaluate_models.py or similar)
+
 python eval_model.py
 
-echo "✅ Model evaluation complete."
+echo "Model evaluation complete."
